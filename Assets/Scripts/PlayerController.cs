fbb82abdde32ba9f6c4c70a0a4cf067787532bbc
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 22:37:50 +0000

    Sep 10, 2022, 6:37 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -76,7 +76,7 @@ public class PlayerController : MonoBehaviour
         else if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
-            count = count - 1;
+            lives = lives - 1;
 
             SetCountText();
         }
