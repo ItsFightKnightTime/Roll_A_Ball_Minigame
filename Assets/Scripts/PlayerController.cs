565cd4fa43eb072442c351c1c221bed1480a1ce6
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 23:28:27 +0000

    Sep 10, 2022, 7:28 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Initial Point Count Value
         rb = GetComponent<Rigidbody>();
         count = 0;
 
