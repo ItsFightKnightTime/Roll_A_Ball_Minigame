45ca032ed9791ed70f633e615d95d1cc1beafa04
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 20:47:16 +0000

    Sep 10, 2022, 4:47 PM

--- Assets/Scripts/MovingWall.cs
+++ Assets/Scripts/MovingWall.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingWall : MonoBehaviour
 {
+    
+    private WaypointPath _waypointPath;
+
     // Start is called before the first frame update
     void Start()
     {
