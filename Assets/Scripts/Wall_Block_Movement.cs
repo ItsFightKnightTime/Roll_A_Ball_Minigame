4816c01c400effd8fb667aa2403811aee97e5ec8
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 19:44:24 +0000

    Sep 10, 2022, 3:44 PM

--- Assets/Scripts/Wall_Block_Movement.cs
+++ Assets/Scripts/Wall_Block_Movement.cs
@@ -4,36 +4,20 @@ using UnityEngine;
 
 public class Wall_Block_Movement : MonoBehaviour
 {
-    // Start and End Markers
-    public Transform startMarker;
-    public Transform endMarker;
+    [SerializeField]
+    private WaypointPath _waypointPath;
 
-    // Movement speed of Wall
-    public float speed = 1.0F;
-
-    // Time when movement started
-    private float startTime;
-
-    //Total distance between start and end markers
-    private float journeyLength;
-
+    [SerializeField]
+    private float _speed;
     // Start is called before the first frame update
     void Start()
     {
-        startTime = Time.time;
-
-        // Journey Length
-        journeyLength = Vector3.Distance(startMarker.position, endMarker.postion);
+     
     }
 
     // Moving to Target end postion
     void Update()
     {
-        // Distance between the two markers and its speed
-        float distCovered = (Time.time - startTime) * speed;
-        float fractionOfJourney = distCovered / journeyLength;
-
-        // Complicated shit
-        transform.position = new Vector3.Lerp(startMarker.postion, endMarker.postion, fractionOfJourney);
+      
     }
 }
