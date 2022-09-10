ecd9b78ba4e5cece116a25a0ab7ca13ac164a599
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 19:18:33 +0000

    Sep 10, 2022, 3:18 PM

--- Assets/Scripts/Wall_Block_Movement.cs
+++ Assets/Scripts/Wall_Block_Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Wall_Block_Movement : MonoBehaviour
 {
+    // Start and End Markers
+    public Transform startMarker;
+    public Transform endMarker;
+    
     // Start is called before the first frame update
     void Start()
     {
