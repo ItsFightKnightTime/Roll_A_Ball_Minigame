8637efab5b6dad2854b9b57cc9fbb813a2242ffa
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 19:38:16 +0000

    Sep 10, 2022, 3:38 PM

--- Assets/Scripts/Wall_Movement.cs
+++ Assets/Scripts/Wall_Movement.cs
@@ -4,4 +4,12 @@ using UnityEngine;
 
 public class Wall_Movement : MonoBehaviour
 {
-   
+   public Transform GetWaypoint(int waypointIndex)
+   {
+        return transform.GetChild(waypointIndex);
+   }
+
+    public int GetNextWaypointIndex(int currentWaypoint)
+    {
+        
+    }
