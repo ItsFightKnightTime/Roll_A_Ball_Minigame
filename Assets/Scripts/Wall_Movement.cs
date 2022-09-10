e50f9e7533da096bbe45d4a4abbe41e6b6c7ef07
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 20:04:06 +0000

    Sep 10, 2022, 4:04 PM

--- Assets/Scripts/Wall_Movement.cs
+++ Assets/Scripts/Wall_Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall_Movement : MonoBehaviour
+public class Wall_Block_Movement : MonoBehaviour
 {
     [SerializeField]
     private WaypointPath _waypointPath;
