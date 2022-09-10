2989a4b532c588d1645d48a81b1a599ce255a551
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 22:35:59 +0000

    Sep 10, 2022, 6:35 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,7 +52,7 @@ public class PlayerController : MonoBehaviour
         }
 
         countText.text = "Lives: " + count.ToString();
-        if (count == 0)
+        if (lives == 0)
         {
             LoseTextObject.SetActive(true);
         }
