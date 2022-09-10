ed38e6275e3efc9c26c472fa2c4a21cda46129ef
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-10 20:59:26 +0000

    Sep 10, 2022, 4:59 PM

--- Assets/Scripts/MovingWall.cs
+++ Assets/Scripts/MovingWall.cs
@@ -32,6 +32,11 @@ public class MovingWall : MonoBehaviour
     private void TargetNextWaypoint()
     {
         _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
-        _targetWaypointIndex = _way
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
     }
 }
