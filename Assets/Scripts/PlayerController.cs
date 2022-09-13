136bf30642f0168bded48dfec44a51ffab0b34a5
ItsFightKnightTime <111759031+ItsFightKnightTime@users.noreply.github.com>
2022-09-13 19:22:44 +0000

    Sep 13, 2022, 3:22 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -91,7 +91,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter (collider other)
+    void OnTriggerEnter(Collider other)
     {
         // Teleportation to new levels
         if (count == 12)
